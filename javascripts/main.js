eedee6441f8b9f23f4cbc6075b8eef9b9ea4fb61
kowus <barnabas@elite-education.org>
2019-02-02 09:39:38 +0000

    magic

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,15 +6,41 @@ window.onload = function(e) {
 
   var quoteBox = document.getElementById('quote-box');
   var quoteButton = document.getElementById('quote-button');
-
+  var quoteInput = document.getElementById('quote-input');
+  var nameInput = document.getElementById('name-input');
+  var magicButton = document.getElementById('magic-button');
   quoteButton.addEventListener('click', e => {
-    fetch('https://api.github.com/zen')
-      .then(response => response.json())
+    fetch('http://api.github.com/zen')
+      .then(response => {
+        return response.text();
+      })
       .then(responseData => {
+        console.log(responseData);
         quoteBox.innerHTML = `<blockquote><q>${responseData}</q></blockquote>`;
+      })
+      .catch(err => {
+        console.error(err);
       });
   });
-
+  magicButton.addEventListener('click', e => {
+    var quote = quoteInput.value;
+    var name = nameInput.value;
+    if (quote.length < 3 || name.length < 2)
+      return alert('Please fill username and quote');
+    fetch('https://radiant-escarpment-11253.herokuapp.com/add-quote', {
+      method: 'POST',
+      body: {
+        quote: quote,
+        username: name
+      }
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(responseData) {
+        console.log(responseData);
+      });
+  });
   add.addEventListener('click', e => {
     var startVal = Number(startPoint.textContent);
     startVal += 1;
